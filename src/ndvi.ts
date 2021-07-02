fbdb844aad24b6c78f3858ffdb91666c4d30d8be
Leonardo Schneider <lnrdschneider@gmail.com>
2021-07-02 18:20:56 -0300

    ndvi.ts ended and json files updated

--- src/ndvi.ts
+++ src/ndvi.ts
@@ -1,14 +1,13 @@
 // Require client library and private key.
 import ee = require('@google/earthengine');
-import devKey = require('./ndvi-316313-00d8329df0f9.json');
 
-let ndviGen: any = (
+export function ndviGen(
   devKey: any,
   polygon: any,
   pixels: number,
   date1: number,
   date2: number
-) => {
+): Promise<any> {
   // -----------------------------------------------------------------------------------------------------
   // ---------------------------- Inicializa o client library e o run analysis ---------------------------
   // -----------------------------------------------------------------------------------------------------
@@ -204,7 +203,7 @@ let ndviGen: any = (
     // -----------------------------------------------------------------------------------------------------
     // -------------------------------------------- Gera objeto --------------------------------------------
     // -----------------------------------------------------------------------------------------------------
-    let myObj:object = {
+    let myObj: object = {
       width: dimensionLng,
       height: dimensionLat,
       centroid: { lng: centroidLng, lat: centroidLat },
@@ -224,38 +223,15 @@ let ndviGen: any = (
   // -----------------------------------------------------------------------------------------------------
   // ------------------------------- Autenticação usando o service account -------------------------------
   // -----------------------------------------------------------------------------------------------------
-
-  ee.data.authenticateViaPrivateKey(devKey, (jorge) => {
-    console.log(runAnalysis());
-    
-  }, function (e: any) {
-    console.error('Authentication error: ' + e);
+  return new Promise(async function (resolve, reject) {
+    return await ee.data.authenticateViaPrivateKey(
+      devKey,
+      () => {
+        resolve(runAnalysis());
+      },
+      (e: any) => {
+        reject('Authentication error: ' + e);
+      }
+    );
   });
-};
-
-const polygon: Array<object> = [
-  {
-    id: 'OTyjcMeO039jfg3illSc',
-    lat: -13.932756478329273,
-    lng: -58.853817994159854
-  },
-
-  {
-    id: 'cLewtvXoyXS0MKaLY3xj',
-    lat: -13.94374420397385,
-    lng: -58.85349532654964
-  },
-
-  {
-    id: '0lKEThC69R1QdAU7dkFk',
-    lat: -13.942169490910729,
-    lng: -58.81596573691317
-  },
-
-  {
-    id: 'XtxciaPwwaDvspCx0Z7W',
-    lat: -13.931257152803115,
-    lng: -58.8162464688904
-  }
-];
-ndviGen(devKey, polygon, 500, 1617753600000, 1620518400000);
+}
